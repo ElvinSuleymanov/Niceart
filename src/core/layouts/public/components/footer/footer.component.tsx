2002e52d4c86f114e4e97a3c35363dcfe68381d0
Javidan -R <rajablijavidan@gmail.com>
2024-04-24 17:39:54 +0400

    feat(layout):Layout footer responsiveness

--- src/core/layouts/public/components/footer/footer.component.tsx
+++ src/core/layouts/public/components/footer/footer.component.tsx
@@ -1,23 +1,23 @@
-import classNames from 'classnames';
-import useLocalization from 'assets/lang';
-import { useMemo } from 'react';
-import { Col, Flex, Row, Typography } from 'antd';
-import { Logo } from 'assets/images/icons/logo';
+import classNames from "classnames";
+import useLocalization from "assets/lang";
+import { useMemo } from "react";
+import { Col, Flex, Row, Typography } from "antd";
+import { Logo } from "assets/images/icons/logo";
 import {
   FacebookIcon,
   InstagramIcon,
   LinkedinIcon,
   TwitterIcon,
   YoutubeIcon,
-} from 'assets/images/icons/socials';
+} from "assets/images/icons/socials";
 import {
   EnvelopeIcon,
   LocationIcon,
   PhoneIcon,
-} from 'assets/images/icons/envelope';
-import { useFooterStyles } from './footer.style';
-import { environment } from 'core/configs/app.config';
-import { Link } from 'react-router-dom';
+} from "assets/images/icons/envelope";
+import { useFooterStyles } from "./footer.style";
+import { environment } from "core/configs/app.config";
+import { Link } from "react-router-dom";
 
 const FooterComponent = () => {
   const classes = useFooterStyles();
@@ -29,8 +29,7 @@ const FooterComponent = () => {
     () =>
       classNames({
         [classes.footer]: true,
-        'py-100': true,
-        
+        "py-100": true,
       }),
     [classes.footer]
   );
@@ -41,71 +40,109 @@ const FooterComponent = () => {
         <Row className={classes.footerSm}>
           <Col lg={6} md={12} xs={24}>
             <Flex vertical gap={20}>
-            <Link  to={"/"}><Logo  /></Link>
-              <p>{translate('footerText')}</p>
-              <Flex gap={15} align='center'>
-               <Link  to={""}><FacebookIcon /></Link>
-               <Link  to={""}><TwitterIcon /></Link>
-               <Link  to={""}><InstagramIcon /></Link>
-               <Link  to={""}><LinkedinIcon /></Link>
-               <Link  to={""}><YoutubeIcon /></Link>
+              <Link to={"/"}>
+                <Logo />
+              </Link>
+              <p>{translate("footerText")}</p>
+              <Flex gap={15} align="center">
+                <Link to={""}>
+                  <FacebookIcon />
+                </Link>
+                <Link to={""}>
+                  <TwitterIcon />
+                </Link>
+                <Link to={""}>
+                  <InstagramIcon />
+                </Link>
+                <Link to={""}>
+                  <LinkedinIcon />
+                </Link>
+                <Link to={""}>
+                  <YoutubeIcon />
+                </Link>
               </Flex>
             </Flex>
           </Col>
 
           <Col lg={6} md={12} xs={24}></Col>
 
-          <Col lg={6} md={12} xs={24}>
+          <Col lg={6} md={12}>
             <Flex vertical gap={15}>
-              <Typography.Title level={5}>{translate('site')}</Typography.Title>
-              <Typography.Link style={{cursor:'pointer', color:'black'}} >{translate('homepage')}</Typography.Link>
-              <Typography.Link style={{cursor:'pointer', color:'black'}} >{translate('aboutUs')}</Typography.Link>
-              <Typography.Link style={{cursor:'pointer', color:'black'}} >{translate('products')}</Typography.Link>
-              <Typography.Link style={{cursor:'pointer', color:'black'}}>{translate('partners')}</Typography.Link>
-              <Typography.Link style={{cursor:'pointer', color:'black'}}>{translate('contact')}</Typography.Link>
+              <Typography.Title level={5}>{translate("site")}</Typography.Title>
+              <Typography.Link style={{ cursor: "pointer", color: "black" }}>
+                {translate("homepage")}
+              </Typography.Link>
+              <Typography.Link style={{ cursor: "pointer", color: "black" }}>
+                {translate("aboutUs")}
+              </Typography.Link>
+              <Typography.Link style={{ cursor: "pointer", color: "black" }}>
+                {translate("products")}
+              </Typography.Link>
+              <Typography.Link style={{ cursor: "pointer", color: "black" }}>
+                {translate("partners")}
+              </Typography.Link>
+              <Typography.Link style={{ cursor: "pointer", color: "black" }}>
+                {translate("contact")}
+              </Typography.Link>
             </Flex>
           </Col>
 
-          <Col lg={6} md={12} xs={24}>
+          <Col lg={6} md={12}>
             <Flex vertical gap={15}>
               <Typography.Title level={5}>
-                {translate('contactsUs')}
+                {translate("contactsUs")}
               </Typography.Title>
-              <Flex gap={10} style={{cursor:'pointer'}}>
+              <Flex gap={10} style={{ cursor: "pointer" }}>
                 <EnvelopeIcon />
                 contact@company.com
               </Flex>
-              <Flex gap={10} style={{cursor:'pointer'}}>
+              <Flex gap={10} style={{ cursor: "pointer" }}>
                 <PhoneIcon />
                 (414) 687 - 5892
               </Flex>
-              <Flex gap={10} style={{cursor:'pointer'}}>
+              <Flex gap={10} style={{ cursor: "pointer" }}>
                 <LocationIcon />
-                {translate('locationOfCompany')}
+                {translate("locationOfCompany")}
               </Flex>
             </Flex>
           </Col>
         </Row>
         <div className={classes.footerBottom}>
-        <Row className="container py-60" >
-          <Col lg={12}>
-            <Typography.Title level={5}>
-              Copyright Ⓒ {date} {projectName}
-            </Typography.Title>
-          </Col>
-          <Col lg={12}>
-            <Flex gap={15}>
-              <Typography>All Rights Reserved</Typography>
-              <Typography style={{color:'#3E6F00', textDecoration:'underline', cursor:'pointer'}}>Terms and Conditions</Typography>
-              <Typography style={{color:'#3E6F00', textDecoration:'underline', cursor:'pointer'}}> Privacy Policy</Typography>
-            </Flex>
-          </Col>
-        </Row>
+          <Row className="container py-60">
+            <Col lg={12}>
+              <Typography.Title level={5}>
+                Copyright Ⓒ {date} {projectName}
+              </Typography.Title>
+            </Col>
+            <Col lg={12}>
+              <Flex gap={15}>
+                <Typography>All Rights Reserved</Typography>
+                <Typography
+                  style={{
+                    color: "#3E6F00",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  }}
+                >
+                  Terms and Conditions
+                </Typography>
+                <Typography
+                  style={{
+                    color: "#3E6F00",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  }}
+                >
+                  {" "}
+                  Privacy Policy
+                </Typography>
+              </Flex>
+            </Col>
+          </Row>
         </div>
-        
       </footer>
     </div>
   );
 };
 
-export default FooterComponent; 
+export default FooterComponent;
