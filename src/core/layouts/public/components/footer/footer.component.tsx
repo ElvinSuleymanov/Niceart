1cac6de352ca4d814a66010fff72dbc5e7fa9ec2
ElvinSuleymanov <elvinsuleymanov150@gmail.com>
2024-04-25 12:10:47 +0400

    feat(layout):responsive footer

--- src/core/layouts/public/components/footer/footer.component.tsx
+++ src/core/layouts/public/components/footer/footer.component.tsx
@@ -1,24 +1,24 @@
-import classNames from "classnames";
-import useLocalization from "assets/lang";
-import { useMemo } from "react";
-import { Col, Flex, Row, Typography } from "antd";
-import { Logo } from "assets/images/icons/logo";
+import classNames from 'classnames';
+import useLocalization from 'assets/lang';
+import { useMemo } from 'react';
+import { Col, Flex, Row, Typography } from 'antd';
+import { Logo } from 'assets/images/icons/logo';
 import {
   FacebookIcon,
   InstagramIcon,
   LinkedinIcon,
   TwitterIcon,
   YoutubeIcon,
-} from "assets/images/icons/socials";
+} from 'assets/images/icons/socials';
 import {
   EnvelopeIcon,
   LocationIcon,
   PhoneIcon,
-} from "assets/images/icons/envelope";
-import { useFooterStyles } from "./footer.style";
-import { environment } from "core/configs/app.config";
-import { Link } from "react-router-dom";
-import { Routes } from "router/routes";
+} from 'assets/images/icons/envelope';
+import { useFooterStyles } from './footer.style';
+import { environment } from 'core/configs/app.config';
+import { Link } from 'react-router-dom';
+import { Routes } from 'router/routes';
 
 const FooterComponent = () => {
   const classes = useFooterStyles();
@@ -30,60 +30,70 @@ const FooterComponent = () => {
     () =>
       classNames({
         [classes.footer]: true,
-        "py-100": true,
+        'pt-50': true,
       }),
     [classes.footer]
   );
 
+  const footerBottomClasses = useMemo(
+    () => (
+      classNames(
+        {
+          'container py-60':true,
+          [classes.footerBottom]:true
+        }
+      )
+    ), [classes.footerBottom]
+  );
   return (
     <div className={classes.footerContainer}>
       <footer className={footerClasses}>
-        <Row className={classes.footerSm}>
-          <Col lg={6} md={12} xs={24} className="my-30">
-            <Flex vertical gap={20}>
+        <Row className={classes.footerSm} >
+          <Col lg={6} md={24} xs={24} className='my-30' >
+            <Flex vertical gap={20} >
               <Link to={Routes.home}>
                 <Logo />
               </Link>
-              <p>{translate("footerText")}</p>
-              <Flex gap={15} align="center">
-                <Link to={"https://www.facebook.com/"}>
+              <p>{translate('footerText')}</p>
+              <Flex gap={15} align='center'>
+                <Link to={'https://www.facebook.com/'}>
                   <FacebookIcon />
                 </Link>
-                <Link to={"https://twitter.com/"}>
+                <Link to={'https://twitter.com/'}>
                   <TwitterIcon />
                 </Link>
-                <Link to={"https://www.instagram.com/"}>
+                <Link to={'https://www.instagram.com/'}>
                   <InstagramIcon />
                 </Link>
-                <Link to={"https://az.linkedin.com/"}>
+                <Link to={'https://az.linkedin.com/'}>
                   <LinkedinIcon />
                 </Link>
-                <Link to={"https://youtube.com/"}>
+                <Link to={'https://youtube.com/'}>
                   <YoutubeIcon />
                 </Link>
               </Flex>
             </Flex>
           </Col>
 
-          <Col lg={6} md={12} xs={24}></Col>
+          <Col lg={6} md={0} ></Col>
 
-          <Col lg={6} md={12}>
-            <Flex vertical gap={15} className="mr-100" align="center">
-              <Typography.Title level={5}>{translate("site")}</Typography.Title>
+          <Col lg={6} md={12} >
+            <Flex vertical gap={15} align='center'>
+              <Typography.Title level={5}>{translate('site')}</Typography.Title>
               <Link to={Routes.home} className={classes.footerLink}>
-                {translate("homepage")}
+                {translate('homepage')}
               </Link>
               <Link to={Routes.about} className={classes.footerLink}>
-                {translate("aboutUs")}
+                {translate('aboutUs')}
               </Link>
               <Link to={Routes.products} className={classes.footerLink}>
-                {translate("products")}
+                {translate('products')}
               </Link>
               <Link to={Routes.partners} className={classes.footerLink}>
-                {translate("partners")}
+                {translate('partners')}
               </Link>
               <Link to={Routes.contact} className={classes.footerLink}>
-                {translate("contact")}
+                {translate('contact')}
               </Link>
             </Flex>
           </Col>
@@ -91,25 +101,25 @@ const FooterComponent = () => {
           <Col lg={6} md={12}>
             <Flex vertical gap={15}>
               <Typography.Title level={5}>
-                {translate("contactsUs")}
+                {translate('contactsUs')}
               </Typography.Title>
-              <Flex gap={10} style={{ cursor: "pointer" }}>
+              <Flex gap={10} style={{ cursor: 'pointer' }}>
                 <EnvelopeIcon />
                 contact@company.com
               </Flex>
-              <Flex gap={10} style={{ cursor: "pointer" }}>
+              <Flex gap={10} style={{ cursor: 'pointer' }}>
                 <PhoneIcon />
                 (414) 687 - 5892
               </Flex>
-              <Flex gap={10} style={{ cursor: "pointer" }}>
+              <Flex gap={10} style={{ cursor: 'pointer' }}>
                 <LocationIcon />
-                {translate("locationOfCompany")}
+                {translate('locationOfCompany')}
               </Flex>
             </Flex>
           </Col>
         </Row>
         <div>
-          <Row className="container py-60">
+          <Row className={footerBottomClasses}>
             <Col lg={12}>
               <Typography.Title level={5}>
                 Copyright Ⓒ {date} {projectName}
--- src/core/layouts/public/components/footer/footer.style.ts
+++ src/core/layouts/public/components/footer/footer.style.ts
@@ -1,8 +1,8 @@
-import { rem } from "assets/styles/abstracts/functions";
-import colors from "assets/styles/abstracts/color";
-import { createUseStyles, Styles } from "react-jss";
-import sizes from "assets/styles/abstracts/sizes";
-import { breakpoint } from "assets/styles/abstracts/mixins";
+import { rem } from 'assets/styles/abstracts/functions';
+import colors from 'assets/styles/abstracts/color';
+import { createUseStyles, Styles } from 'react-jss';
+import sizes from 'assets/styles/abstracts/sizes';
+import { breakpoint } from 'assets/styles/abstracts/mixins';
 const styles: Styles = {
   footerContainer: {
     marginTop: rem(250),
@@ -19,22 +19,36 @@ const styles: Styles = {
   },
   footerSm: {
     maxWidth: rem(1240),
-    margin: "0 auto",
-    padding: "0 15px",
+    margin: '0 auto',
+    padding: '0 15px',
     [breakpoint(768)]: {
-      display: "flex",
-      justifyContent: "center",
-      textAlign: "left",
+      display: 'flex',
+      justifyContent: 'center',
+      gap:rem(20)
     },
+    [breakpoint(992)]: {
+      '& .ant-col': {
+        '& .ant-flex': {
+          alignItems:'center'
+        }
+      }
+    }
   },
   footerLink: {
-    cursor: "pointer",
+    cursor: 'pointer',
     color: colors.textMain,
+    textAlign:'center'
   },
   footerCopy: {
     color: colors.greenMain,
-    textDecoration: "underline",
+    textDecoration: 'underline',
   },
+  footerBottom: {
+    [breakpoint(992)]: {
+      justifyContent:'center',
+      gap:rem(20)
+    }
+  }
 };
 
 export const useFooterStyles = createUseStyles(styles);
